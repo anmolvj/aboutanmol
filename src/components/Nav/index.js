301895de2e08f1183539df7151ef662bd36bc89f
Anmol Vijayvargiya <anmol.vijay94@gmail.com>
2020-05-28 21:06:30 -0700

    Implement nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,41 +1,96 @@
 import React from "react"
+import Drawer from "@material-ui/core/Drawer"
 import AppBar from "@material-ui/core/AppBar"
 import Toolbar from "@material-ui/core/Toolbar"
+import List from "@material-ui/core/List"
+import Typography from "@material-ui/core/Typography"
+import Divider from "@material-ui/core/Divider"
+import IconButton from "@material-ui/core/IconButton"
+import MenuIcon from "@material-ui/icons/Menu"
+import ListItem from "@material-ui/core/ListItem"
+import ListItemIcon from "@material-ui/core/ListItemIcon"
+import ListItemText from "@material-ui/core/ListItemText"
+import InboxIcon from "@material-ui/icons/MoveToInbox"
+import MailIcon from "@material-ui/icons/Mail"
 import styled from "styled-components"
-import Grid from "@material-ui/core/Grid"
 
 import Name from "./Name"
-import NavDrawer from "./Drawer/index"
+import { StyledAppBar, StyledToolbar, StyledDrawerPaper } from "./styled"
 
-const StyledAppBar = styled(AppBar)`
-  background-color: ${props => props.theme.colors.darkerGrey};
+const StyledContainer = styled.div`
+  display: flex;
 `
 
-const StyledGridContainer = styled(Grid)`
-  min-width: 100%;
-`
-const Container = ({ children }) => (
-  <StyledAppBar position="static">
-    <Toolbar>{children}</Toolbar>
-  </StyledAppBar>
-)
+const Nav = ({ children }) => {
+  const [state, setState] = React.useState({
+    isDrawerOpen: false,
+  })
 
-const Nav = () => (
-  <Container>
-    <StyledGridContainer
-      container
-      direction="row"
-      justify="space-between"
-      alignItems="center"
-    >
-      <Grid item>
-        <Name />
-      </Grid>
-      <Grid item xs="1">
-        <NavDrawer />
-      </Grid>
-    </StyledGridContainer>
-  </Container>
-)
+  const toggleDrawer = open => event => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return
+    }
+
+    setState({ ...state, isDrawerOpen: open })
+  }
+
+  return (
+    <StyledContainer>
+      <StyledAppBar position="fixed">
+        <StyledToolbar variant="dense">
+          <Typography variant="h6" noWrap>
+            <Name />
+          </Typography>
+          <IconButton
+            disableFocusRipple
+            disableRipple
+            edge="end"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={toggleDrawer(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+        </StyledToolbar>
+      </StyledAppBar>
+
+      {/* {children} */}
+
+      <Drawer
+        anchor="right"
+        open={state["isDrawerOpen"]}
+        onClose={toggleDrawer(false)}
+        PaperProps={{
+          component: StyledDrawerPaper,
+        }}
+      >
+        <List>
+          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {["All mail", "Trash", "Spam"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </StyledContainer>
+  )
+}
 
 export default Nav
--- src/components/layouts/home.js
+++ src/components/layouts/home.js
@@ -1,39 +1,23 @@
 import React from "react"
 import PropTypes from "prop-types"
-import "normalize.css"
-import Grid from "@material-ui/core/Grid"
-import { Container } from "@material-ui/core"
-import styled from "styled-components"
+import CssBaseline from "@material-ui/core/CssBaseline"
 
 import { StyledComponentsTheme, MuiTheme } from "../theme"
 import Nav from "../Nav"
-
-const Wrapper = styled(Container)`
-  background-color: ${props => props.theme.colors.darkGrey};
-  min-height: 100vh;
-  padding: 0;
-`
 
 const HomeLayout = ({ children }) => {
   if (typeof window !== "undefined") {
     require("smooth-scroll")('a[href*="#"]')
   }
   return (
-    <StyledComponentsTheme>
-      <MuiTheme>
-        <Wrapper maxWidth="false">
-          <Grid container spacing="5">
-            <Grid item xs={12}>
-              <Nav />
-            </Grid>
-
-            <Grid item xs={12}>
-              <Container>{children}</Container>
-            </Grid>
-          </Grid>
-        </Wrapper>
-      </MuiTheme>
-    </StyledComponentsTheme>
+    <React.Fragment key="parent">
+      <CssBaseline />
+      <StyledComponentsTheme>
+        <MuiTheme>
+          <Nav>{children}</Nav>
+        </MuiTheme>
+      </StyledComponentsTheme>
+    </React.Fragment>
   )
 }
 
